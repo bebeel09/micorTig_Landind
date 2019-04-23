add8c6d883b0a038637cea919d1deb167d57be52
Айдар Халитов <ajdarhalitiv2622@gmail.com>
2019-04-23 18:45:21 +0500

    new content block

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,18 @@ var gulp      = require('gulp'), // Подключаем Gulp
 	concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
 	uglify      = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
 	cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+	rename      = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
 	del         = require('del'); // Подключаем библиотеку для удаления файлов и папок
+	imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+	pngquant    = require('imagemin-pngquant'); // Подключаем библиотеку для работы с png
+	cache       = require('gulp-cache'); // Подключаем библиотеку кеширования
 	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 	gulp.task('sass', function(){ // Создаем таск Sass
 		return gulp.src('app/sass/**/*.sass') // Берем источник
 			.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
 			.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-			.pipe(gulp.dest('dist/css')) // Выгружаем результата в папку app/css
+			.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 			.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 	});
 
@@ -54,10 +58,16 @@ gulp.task('clean', function() {
 
 gulp.task('img', function() {
 	return gulp.src('app/img/**/*') // Берем все изображения из app
+		.pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
 		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
 });
 
-gulp.task('build', ['clean','img', 'sass', 'scripts'], function() {
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
 
 	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
 		'app/css/main.css',
