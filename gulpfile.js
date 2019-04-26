b44101861e6de39f50d430293106d1b67b2168d0
Shikalov Nikita <frolovnika@gmail.com>
2019-04-26 10:35:24 +0500

    start sublime parallel gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 var gulp      = require('gulp'), // Подключаем Gulp
 	sass        = require('gulp-sass'), //Подключаем Sass пакет,
-	browserSync = require('browser-sync'); // Подключаем Browser Sync
+	browserSync = require('browser-sync'), // Подключаем Browser Sync
 	concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-	uglify      = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
+	uglify      = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
 	cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-	rename      = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
-	del         = require('del'); // Подключаем библиотеку для удаления файлов и папок
+	rename      = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+	del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
 	imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-	pngquant    = require('imagemin-pngquant'); // Подключаем библиотеку для работы с png
-	cache       = require('gulp-cache'); // Подключаем библиотеку кеширования
-	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+	pngquant    = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+	cache       = require('gulp-cache'), // Подключаем библиотеку кеширования
+	autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
+	exec 		= require('executive');
 
-	gulp.task('sass', function(){ // Создаем таск Sass
-		return gulp.src('app/sass/**/*.sass') // Берем источник
-			.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-			.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-			.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-			.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-	});
+exec('subl %CD%');
+
+gulp.task('sass', function(){ // Создаем таск Sass
+	return gulp.src('app/sass/**/*.sass') // Берем источник
+		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
 
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -93,4 +96,4 @@ gulp.task('default', ['watch']);
 
 gulp.task('clear', function () {
 	return cache.clearAll();
-})
+});
